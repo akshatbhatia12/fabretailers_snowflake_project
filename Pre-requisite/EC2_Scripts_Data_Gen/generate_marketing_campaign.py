4c49bab7d4fcba5eec9a6fdb41f59155bfa18e32
akshatbhatia12 <119995818+akshatbhatia12@users.noreply.github.com>
2025-04-28 07:52:04 +0530

    Create generate_marketing_campaign.py

---
+++ Pre-requisite/EC2_Scripts_Data_Gen/generate_marketing_campaign.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import random
+from faker import Faker
+import boto3
+from io import StringIO
+
+# AWS S3 configuration
+s3_client = boto3.client('s3')
+bucket_name = 'snowflake-fabretailers-project'
+folder_path = 'sales_marketing/marketing_campaigns/'
+
+fake = Faker()
+
+# Generating marketing campaigns data
+marketing_campaigns = []
+for i in range(50):  # Number of campaigns
+    campaign_id = f"C{str(i+1).zfill(3)}"
+    campaign_name = random.choice(['Spring Sale', 'Winter Promotion', 'Back to School', 'Black Friday Sale'])
+    start_date = fake.date_this_year()
+    end_date = fake.date_this_year()
+    budget = random.randint(50000, 100000)
+    status = random.choice(['Active', 'Completed', 'Planned'])
+    store_ids = random.choice(['ST001', 'ST002', 'ST003','ST004','ST005'])
+    marketing_campaigns.append([campaign_id, campaign_name, start_date, end_date, budget, status,store_ids])
+
+# Save to CSV in memory
+df_campaigns = pd.DataFrame(marketing_campaigns, columns=['campaign_id', 'campaign_name', 'start_date', 'end_date', 'budget', 'status','store_ids'])
+csv_buffer = StringIO()
+df_campaigns.to_csv(csv_buffer, index=False)
+
+# Upload to S3
+s3_client.put_object(Bucket=bucket_name, Key=folder_path, Body=csv_buffer.getvalue())
+
+print(f"marketing_campaigns.csv uploaded to {bucket_name}/{folder_path}.")
